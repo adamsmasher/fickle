4a32df54b7429a3d9fc4415fe01e988f1a3fc588
Adam Smith <adam@tablethotels.com>
2012-08-21 12:02:27 -0400

    add a new test (regarding globals)
    Signed-off-by: Adam Smith <adam@tablethotels.com>

--- tests/test_fickle.py
+++ tests/test_fickle.py
@@ -5,9 +5,23 @@ import unittest
 def module_scope_add_10(x):
     return x + 10
 
+g = 10
+def module_scope_add_global(x):
+    return x + g
+
 class TestFickle(unittest.TestCase):
     def test_module_scope(self):
         f = fickle.loads(fickle.dumps(module_scope_add_10))
+        self.assertEqual(f(10), 20)
+
+    def test_module_scope_read_global(self):
+        '''What should we do when we reference a global? It's something we
+           closed over, so let's try to keep the value.'''
+        s = fickle.dumps(module_scope_add_global)
+        # remove g from the global dictionary to simulate loading in a different
+        # time and place
+        del globals()['g']
+        f = fickle.loads(s)
         self.assertEqual(f(10), 20)
 
 if __name__ == '__main__':
