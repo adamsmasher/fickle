4612f0a19ba965b6e3062814d654352ae33ce1d4
Adam Smith <adam@tablethotels.com>
2012-08-22 18:36:18 -0400

    support closures
    Signed-off-by: Adam Smith <adam@tablethotels.com>

--- fickle.py
+++ fickle.py
@@ -1,8 +1,32 @@
 import marshal
+import pickle
 import types
 
 def dumps(f):
-    return marshal.dumps(f.func_code)
+    code = marshal.dumps(f.func_code)
+    closure = ([cell.cell_contents for cell in f.func_closure]
+               if f.func_closure else None)
+    return pickle.dumps((code, closure))
+
 
 def loads(f):
-    return types.FunctionType(marshal.loads(f), globals())
+    code, closure = pickle.loads(f)
+
+    code = marshal.loads(code)
+
+    if closure:
+        closure = _instantiate_closure(closure)
+
+    return types.FunctionType(code, globals(), closure=closure)
+
+
+def _instantiate_closure(closure):
+    '''Consumes a list of (varname, value) pairs and produces a tuple of cells
+       containing the values.'''
+    return tuple(_new_cell(value) for value in closure)
+
+
+def _new_cell(contents):
+    def f():
+        return contents
+    return f.func_closure[0]
